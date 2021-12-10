96682d52e235064efad67e767b3a79c69c141c88
Cerbrus <michiel1900@gmail.com>
2021-12-10 10:02:44 +0100

    4.2
    Man that didn't cooperate...

--- 2021/04.js
+++ 2021/04.js
@@ -1,8 +1,11 @@
-// 4.1
+// Setup
 document.getElementsByTagName('pre')[0].innerText.split(/\n\n/)
 
 class Board {
   static row = [0,1,2,3,4];
+  
+  win = false;
+  score = 0;
 
   constructor(input) {
     this.input = input;
@@ -10,19 +13,20 @@ class Board {
   }
   
   getScore(draw) {
+    this.win = false;
     for(let i = 0; i < 5; i++) {
-      let win = false;
       if(Board.row.every(c => draw.includes(this.cells[c + i * 5]))) {
-         console.log(`Winning board:\n${this.input}\nRow ${i}`);
-         win = true;
+         // console.log(`Winning board:\n${this.input}\nRow ${i}`);
+         this.win = true;
       }
       if(Board.row.every(c => draw.includes(this.cells[c * 5 + i]))) {
-         console.log(`Winning board:\n${this.input}\nColumn ${i}`);
-         win = true;
+         // console.log(`Winning board:\n${this.input}\nColumn ${i}`);
+         this.win = true;
       }
 
-      if(win) {
-        return this.cells.filter(cell => !draw.includes(cell))
+      if(this.win) {
+        this.score = this.cells.filter(cell => !draw.includes(cell));
+        return this.score;
       }
     }
   }
@@ -32,6 +36,7 @@ var [drawn, ...boards] = document.getElementsByTagName('pre')[0].innerText.split
 drawn = drawn.split(',').map(Number);
 boards = boards.map(b => new Board(b));
 
+// 4.1
 for(let n = 5; n < drawn.length; n++) {
   const draw = drawn.slice(0, n);
   const scores = boards.map(b => b.getScore(draw)).filter(Boolean);
@@ -41,6 +46,23 @@ for(let n = 5; n < drawn.length; n++) {
     break;
   }
 }
+// 33462
+
+// 4.2
+for(let n = 5; n < drawn.length; n++) {
+  const draw = drawn.slice(0, n);
+  boards.forEach(b => b.getScore(draw))
+  if(boards.length > 1) {
+    boards = boards.filter(board => !board.win);
+  }
+  if(boards.length === 1 && boards[0].win) {
+    boards[0].getScore(drawn.slice(0, n + 1));
+    const score = boards[0].score.reduce((a,b)=>a+b) * draw.at(-1);
+    console.log(n, 'score: ', score);
+    break;
+  }
+}
+// 30070
 
 //  0  1  2  3  4
 //  5  6  7  8  9
