f1a4fa47a139ec830b5ed79c174807ecc09c5f11
Cerbrus <Cerbrus@users.noreply.github.com>
2022-12-16 16:05:13 +0100

    Create 05.js

---
+++ 2022/05.js
@@ -0,0 +1,42 @@
+// 5.1
+const stacks = `BVSNTCHQ
+WDBG
+FWRTSQB
+LGWSZJDN
+MPDVF
+FWJ
+LNQBJV
+GTRCJQSN
+JSQCWDM`.split('\n').map(s => [...s])
+
+document.body.textContent.trim().split('\n')
+    .slice(10)
+    .map(line => line.match(/(\d+)/g).map(Number))
+    .forEach(([count, origin, target]) => {
+        stacks[target - 1].push(...stacks[origin - 1].splice(-count).reverse());
+    });
+
+stacks.map(s => s.pop()).join('');
+// 'FJSRQCFTN'
+
+// 5.2
+const stacks = `BVSNTCHQ
+WDBG
+FWRTSQB
+LGWSZJDN
+MPDVF
+FWJ
+LNQBJV
+GTRCJQSN
+JSQCWDM`.split('\n').map(s => [...s])
+
+const instructions = document.body.textContent.trim().split('\n')
+    .slice(10)
+    .map(line => line.match(/(\d+)/g).map(Number));
+
+instructions.forEach(([count, origin, target]) => {
+   stacks[target - 1].push(...stacks[origin - 1].splice(-count));
+});
+
+stacks.map(s => s.pop()).join('');
+// 'CJVLJQPHS'
