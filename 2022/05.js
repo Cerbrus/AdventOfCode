4ce202578443f99611c3325c2127aa4803ac77d5
Cerbrus <Cerbrus@users.noreply.github.com>
2022-12-23 09:04:52 +0100

    Update 05.js

--- 2022/05.js
+++ 2022/05.js
@@ -20,23 +20,12 @@ stacks.map(s => s.pop()).join('');
 // 'FJSRQCFTN'
 
 // 5.2
-const stacks = `BVSNTCHQ
-WDBG
-FWRTSQB
-LGWSZJDN
-MPDVF
-FWJ
-LNQBJV
-GTRCJQSN
-JSQCWDM`.split('\n').map(s => [...s])
-
-const instructions = document.body.textContent.trim().split('\n')
+document.body.textContent.trim().split('\n')
     .slice(10)
-    .map(line => line.match(/(\d+)/g).map(Number));
-
-instructions.forEach(([count, origin, target]) => {
-   stacks[target - 1].push(...stacks[origin - 1].splice(-count));
-});
+    .map(line => line.match(/(\d+)/g).map(Number))
+    .forEach(([count, origin, target]) => {
+        stacks[target - 1].push(...stacks[origin - 1].splice(-count));
+    });
 
 stacks.map(s => s.pop()).join('');
 // 'CJVLJQPHS'
