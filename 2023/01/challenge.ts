2b575671eaf479ea481ce0f0b6edbbb627066121
Cerbrus <michiel1900@gmail.com>
2023-12-05 23:22:08 +0100

    Day 1 & 2
    WTF day one...

--- 2023/01/challenge.ts
+++ 2023/01/challenge.ts
@@ -2,21 +2,43 @@ import * as console from 'console';
 import { ChallengeBase, IAnswer } from '../challenge.base';
 
 interface IParseResult {
+    lines: Array<ILine>;
+}
+
+interface ILine {
+    line: string;
+    calibrationValue: number;
 }
 
 class Challenge extends ChallengeBase<IParseResult> {
-    constructor() {
-        super(true);
-    }
+    private readonly numberMap = {
+        oneight: '18', threeight: '38', fiveight: '58', nineight: '98',
+        twone: '21',
+        sevenine: '79', eightwo: '82', eightree: '83',
+        one: '1', two: '2', three: '3', four: '4', five: '5', six: '6', seven: '7', eight: '8', nine: '9'
+    };
+
+    private readonly numberMapRegex = new RegExp(Object.keys(this.numberMap).join('|'), 'g');
 
     protected parseInput(inputString: string): IParseResult {
-        return {};
+        const lines = inputString
+            .split(/[\r\n]+/g)
+            .map(l => l.replace(this.numberMapRegex, (match) => this.numberMap[match]))
+            .map(line => {
+                const digits = line.match(/\d/g);
+                return {
+                    line,
+                    calibrationValue: Number(digits[0] + digits[digits.length - 1])
+                };
+            });
+
+        return { lines };
     }
 
     protected processInput(data: IParseResult): IAnswer {
-        const answerOne = 0;
+        const answerOne = data.lines.reduce((total, line) => total + line.calibrationValue, 0);
 
-        const answerTwo = 0;
+        const answerTwo = answerOne;
 
         return {
             one: answerOne,
--- 2023/02/challenge.ts
+++ 2023/02/challenge.ts
@@ -2,26 +2,74 @@ import * as console from 'console';
 import { ChallengeBase, IAnswer } from '../challenge.base';
 
 interface IParseResult {
+    games: Array<IGame>;
 }
 
-class Challenge extends ChallengeBase<IParseResult> {
-    constructor() {
-        super(true);
-    }
+type TColor = 'red' | 'green' | 'blue';
 
+interface IColorCount extends Record<TColor, number> {
+}
+
+interface IGame extends IColorCount {
+    id: number;
+}
+
+const CUBE_COUNT: IColorCount = {
+    red: 12,
+    green: 13,
+    blue: 14
+};
+
+class Challenge extends ChallengeBase<IParseResult> {
     protected parseInput(inputString: string): IParseResult {
-        return {};
+        const games = inputString
+            .split(/[\r\n]+/g)
+            .map(this.parseGame);
+
+        return { games };
     }
 
     protected processInput(data: IParseResult): IAnswer {
-        const answerOne = 0;
-
-        const answerTwo = 0;
+        const answerOne = this.addPossibleGames(data.games, CUBE_COUNT);
+        console.log(data.games[0]);
+        const answerTwo = this.calculatePower(data.games);
 
         return {
             one: answerOne,
             two: answerTwo
         };
+    }
+
+    private addPossibleGames(games: Array<IGame>, criteria: IColorCount): number {
+        return games
+            .filter((game) =>
+                game.red <= criteria.red &&
+                game.green <= criteria.green &&
+                game.blue <= criteria.blue
+            )
+            .reduce((total, game) => total + game.id, 0);
+    }
+
+    private parseGame(line: string): IGame {
+        const [gameId, subsetString] = line.split(/: +/);
+        const id = Number(gameId.match(/\d+/));
+        const game: IGame = { id, red: 0, green: 0, blue: 0 };
+
+        subsetString
+            .split('; ')
+            .forEach((gameString) => {
+                gameString.split(', ')
+                    .forEach((hand) => {
+                        const [count, color] = hand.split(' ');
+                        game[color] = Math.max(Number(count), game[color]);
+                    });
+            }, {});
+
+        return game;
+    }
+
+    private calculatePower(games: Array<IGame>): number {
+        return games.reduce((result, game) => result + game.red * game.green * game.blue, 0);
     }
 }
 
