74ff9492c3c8620821b777337abe8e0607d096af
Cerbrus <michiel1900@gmail.com>
2023-12-08 07:23:09 +0100

    Prepare day 09-12

---
+++ 2023/10/challenge.ts
@@ -0,0 +1,29 @@
+import * as console from 'console';
+import { ChallengeBase, IAnswer } from '../challenge.base';
+
+interface IParseResult {
+}
+
+class Challenge extends ChallengeBase<IParseResult> {
+    constructor() {
+        super(true);
+    }
+
+    protected parseInput(inputString: string): IParseResult {
+        const lines = inputString.split(/[\r\n]+/);
+        return {};
+    }
+
+    protected processInput(data: IParseResult): IAnswer {
+        const answerOne = 0;
+
+        const answerTwo = 0;
+
+        return {
+            one: answerOne,
+            two: answerTwo
+        };
+    }
+}
+
+new Challenge();
